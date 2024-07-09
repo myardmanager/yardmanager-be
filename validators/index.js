387fbe127bde7d3201c89bc3189b83e347845b3e
mashaimtahir <tmashaim@gmail.com>
2024-07-09 17:07:43 +0500

    Validator fix

--- validators/index.js
+++ validators/index.js
@@ -5,7 +5,7 @@ exports.runValidation = (req, res, next) => {
 	if (!errors.isEmpty()) {
 		return res.status(422).json({
 			success: false,
-			error: errors.array()[0].msg
+			message: errors.array()[0].msg
 		});
 	}
 	next();
