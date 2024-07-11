6d0be5b31e57218508e4b51436a3f85832b13725
mashaimtahir <tmashaim@gmail.com>
2024-07-12 00:26:21 +0500

    AWS S3 Configured

--- services/backblaze.service.js
+++ services/backblaze.service.js
@@ -43,8 +43,8 @@
 const aws = require("aws-sdk");
 
 aws.config.update({
-	accessKeyId: 'AKIAZI2LHOVK43G6IZ6Z',
-	secretAccessKey: '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'
+	accessKeyId: process.env.AWS_KEY_ID,
+	secretAccessKey: process.env.AWS_APP_KEY
 });
 
 const s3 = new aws.S3();
