3cdc20f847628acbb34af9f9ba92633371043dac
mashaimtahir <tmashaim@gmail.com>
2024-07-25 12:03:26 +0500

    Changes to be committed: 	modified:   models/company.model.js

--- models/company.model.js
+++ models/company.model.js
@@ -30,6 +30,14 @@ const companySchema = new mongoose.Schema(
 			type: mongoose.Schema.Types.ObjectId,
 			ref: "User",
 			required: true
+		},
+		price: {
+			type: Boolean,
+			default: false
+		},
+		image: {
+			type: Boolean,
+			default: false
 		}
 	},
 	{ timestamps: true }
