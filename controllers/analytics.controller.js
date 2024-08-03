9e45d82336bd6ee915e7896c6be7facfe7fa3dfc
mashaimtahir <tmashaim@gmail.com>
2024-08-03 17:13:13 +0500

    Analytics graph universal

--- controllers/analytics.controller.js
+++ controllers/analytics.controller.js
@@ -6,85 +6,86 @@ const Location = require("../models/location.model");
 const { default: mongoose } = require("mongoose");
 
 exports.getDashboardAnalytics = async (req, res) => {
-	try {
-		const employees = await Employee.countDocuments({ company: req.user.company });
-		const inventories = await Inventory.countDocuments({ company: req.user.company, deleted: false });
-		const vehicles = await Vehicle.countDocuments({ company: req.user.company });
-		const parts = await Part.countDocuments({ company: req.user.company });
-		const locations = await Location.countDocuments({ company: req.user.company });
+  try {
+    const employees = await Employee.countDocuments({
+      company: req.user.company,
+    });
+    const inventories = await Inventory.countDocuments({
+      company: req.user.company,
+      deleted: false,
+    });
+    const vehicles = await Vehicle.countDocuments({
+      company: req.user.company,
+    });
+    const parts = await Part.countDocuments({ company: req.user.company });
+    const locations = await Location.countDocuments({
+      company: req.user.company,
+    });
 
-		const analytics = {
-			employees: employees,
-			inventories: inventories,
-			parts: parts,
-			vehicles: vehicles,
-			locations: locations
-		};
+    const analytics = {
+      employees: employees,
+      inventories: inventories,
+      parts: parts,
+      vehicles: vehicles,
+      locations: locations,
+    };
 
-		res.status(200).json(analytics);
-	} catch (err) {
-		console.log(err);
-		res.status(500).json({ msg: "Internal server error" });
-	}
+    res.status(200).json(analytics);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ msg: "Internal server error" });
+  }
 };
 
 function getWeekNumber(date) {
-	const firstDayOfYear = new Date(date.getFullYear(), 0, 1);
-	const pastDaysOfYear = (date - firstDayOfYear) / 86400000;
+  const firstDayOfYear = new Date(date.getFullYear(), 0, 1);
+  const pastDaysOfYear = (date - firstDayOfYear) / 86400000;
 
-	return Math.ceil((pastDaysOfYear + firstDayOfYear.getDay() + 1) / 7);
+  return Math.ceil((pastDaysOfYear + firstDayOfYear.getDay() + 1) / 7);
 }
-
 
 // Get Inventory data ordered by date with filter option
 exports.getInventoryData = async (req, res) => {
-	try {
-		// const { division } = req.query;
-		// let filter = {};
-		// if (division === "month") {
-		// 	filter = { createdAt: { $gte: new Date(new Date().getFullYear(), new Date().getMonth(), 1) } };
-		// } else if (division === "year") {
-		// 	filter = { createdAt: { $gte: new Date(new Date().getFullYear(), 0, 1) } };
-		// } else {
-		// 	filter = { createdAt: { $gte: new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate()) } };
-		// }
-    let companyId = new mongoose.Types.ObjectId(req.user.company);
-		const inventory = await Vehicle.find({ company: companyId })
-			.sort({ createdAt: 1 })
-			.then((inventory) => {
-				const data = inventory.map((item) => {
-					return {
-						year: item.createdAt.getFullYear(),
-						month: item.createdAt.getMonth() + 1,
-						day: item.createdAt.getDate(),
-						week: getWeekNumber(item.createdAt),
-						count: 1
-					};
-				});
-				return data;
-			});
+  try {
+    // let companyId = new mongoose.Types.ObjectId(req.user.company);
+    let companyId = "";
+    if (req.user.type === "admin" && req.query.division === "company") {
+      companyId = {};
+    } else {
+      companyId = new mongoose.Types.ObjectId(req.user.company);
+    }
+    const inventory = await Vehicle.find(companyId)
+      .sort({ createdAt: 1 })
+      .then((inventory) => {
+        const data = inventory.map((item) => {
+          return {
+            year: item.createdAt.getFullYear(),
+            month: item.createdAt.getMonth() + 1,
+            day: item.createdAt.getDate(),
+            week: getWeekNumber(item.createdAt),
+            count: 1,
+          };
+        });
+        return data;
+      });
 
-		console.log(inventory);
-		res.status(200).json(inventory);
-	} catch (err) {
-		console.log(err);
-		res.status(500).json({ msg: "Internal server error" });
-	}
+    console.log(inventory);
+    res.status(200).json(inventory);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ msg: "Internal server error" });
+  }
 };
 
 exports.getPartData = async (req, res) => {
   try {
-    // const { division } = req.query;
-    // let filter = {};
-    // if (division === "month") {
-    //   filter = { createdAt: { $gte: new Date(new Date().getFullYear(), new Date().getMonth(), 1) } };
-    // } else if (division === "year") {
-    //   filter = { createdAt: { $gte: new Date(new Date().getFullYear(), 0, 1) } };
-    // } else {
-    //   filter = { createdAt: { $gte: new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate()) } };
-    // }
-    let companyId = new mongoose.Types.ObjectId(req.user.company);
-    const part = await Part.find({ company: companyId })
+    let companyId = "";
+    if (req.user.type === "admin" && req.query.division === "company") {
+      companyId = {};
+    } else {
+      companyId = new mongoose.Types.ObjectId(req.user.company);
+    }
+    const part = await Part.find(companyId)
       .sort({ createdAt: 1 })
       .then((part) => {
         const data = part.map((item) => {
@@ -92,8 +93,8 @@ exports.getPartData = async (req, res) => {
             year: item.createdAt.getFullYear(),
             month: item.createdAt.getMonth() + 1,
             day: item.createdAt.getDate(),
-						week: getWeekNumber(item.createdAt),
-            count: 1
+            week: getWeekNumber(item.createdAt),
+            count: 1,
           };
         });
         return data;
