321c278b52e6df4b31e75e3e7f92f8a161cf77c8
mashaimtahir <tmashaim@gmail.com>
2024-08-03 19:11:24 +0500

    Admin name fix

--- models/admin.model.js
+++ models/admin.model.js
@@ -1,32 +1,35 @@
 const mongoose = require("mongoose");
 
 const adminSchema = new mongoose.Schema(
-	{
-		name: {
-			type: String,
-			required: true
-		},
-		email: {
-			type: String,
-			required: true,
-			unique: true
-		},
-		password: {
-			type: String,
-			required: true,
-			select: false
-		},
-		profile: {
-			type: String
-		},
-		profile: {
-			type: String
-		},
-		cover: {
-			type: String
-		}
-	},
-	{ timestamps: true }
+  {
+    name: {
+      first: {
+        type: String,
+        required: true,
+      },
+      last: {
+        type: String,
+        required: true,
+      },
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      select: false,
+    },
+    profile: {
+      type: String,
+    },
+    cover: {
+      type: String,
+    },
+  },
+  { timestamps: true }
 );
 
 module.exports = mongoose.model("Admin", adminSchema);
