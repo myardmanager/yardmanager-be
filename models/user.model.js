109a6133fde80fd543aa3301db0365b555dd6795
mashaimtahir <tmashaim@gmail.com>
2024-08-06 02:07:20 +0500

    Changes to be committed: 	modified:   models/user.model.js

--- models/user.model.js
+++ models/user.model.js
@@ -1,42 +1,41 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema(
-	{
-		name: {
-			first: {
-				type: String,
-				required: true
-			},
-			last: {
-				type: String,
-				required: true
-			}
-		},
-		username: {
-			type: String,
-			default: "",
-			unique: false
-		},
-		email: {
-			type: String,
-			required: true,
-			unique: true
-		},
-		password: {
-			type: String,
-			required: true
-		},
-		profile: {
-			type: String
-		},
-		lastLogin: {
-			type: Date
-		}
-	},
-	{ timestamps: true }
+  {
+    name: {
+      first: {
+        type: String,
+        required: true,
+      },
+      last: {
+        type: String,
+        required: true,
+      },
+    },
+    username: {
+      type: String,
+      default: "",
+      unique: false,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    profile: {
+      type: String,
+    },
+    lastLogin: {
+      type: Date,
+    },
+  },
+  { timestamps: true }
 );
 
-userSchema.clearIndexes();
-userSchema.index(false);
-
-module.exports = mongoose.model("User", userSchema);
+module.exports = mongoose
+  .model("User", userSchema)
+  .collection.dropIndex({ username: 1 });
