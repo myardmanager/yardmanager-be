1e144da5a41d40bcbd042a7ef60112be4ed886b1
mashaimtahir <tmashaim@gmail.com>
2024-08-10 18:45:57 +0500

    Location limit set to 1

--- validators/location.validator.js
+++ validators/location.validator.js
@@ -6,6 +6,6 @@ exports.validateLocation = [
     .withMessage("Location is required")
     .isString()
     .withMessage("Location must be a string")
-    .isLength({ min: 5 })
+    .isLength({ min: 1 })
     .withMessage("Location must be at least 5 characters long")
 ];
