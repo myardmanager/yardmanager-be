41df996edd663d6b915dbac5f6f7896157ce891c
mashaimtahir <tmashaim@gmail.com>
2024-08-30 00:06:20 +0500

    Fix

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -206,8 +206,8 @@ exports.updateInfo = async (req, res) => {
       const salt = await bcrypt.genSalt(10);
       req.body.password = await bcrypt.hash(req.body.password, salt);
     }
-    req.body.email = undefined;
-    const user = await User.findByIdAndUpdate(userId, req.body, {
+    const {email, ...body} = req.body;
+    const user = await User.findByIdAndUpdate(userId, body, {
       new: true,
     });
     if (!user) {
